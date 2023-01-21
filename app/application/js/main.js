9c7bfa1deca38e6cf11aa51b341b0d3e65f58100
Tom <tom54699@gmail.com>
2023-01-21 19:25:30 +0800

    Feat: 左右區塊縮放    1/21

--- app/application/js/main.js
+++ app/application/js/main.js
@@ -7,6 +7,7 @@ window.addEventListener("load", async () => {
 })
 window.addEventListener("DOMContentLoaded", () => {
     checkMainPageAuth()
+    windowTabContainerAddEvent()
 })
 // 檢查權限
 async function checkMainPageAuth() {
@@ -54,7 +55,7 @@ logoutButton[0].addEventListener("click", async () => {
         location.href = "/auth"
     }
 })
-// 畫面
+/* 畫面 */
 let isTagFilter = false
 const tagFilterButton = document.getElementsByClassName("middleSection-nav-button")
 tagFilterButton[0].addEventListener("click", () => {
@@ -120,17 +121,105 @@ const windowTabsCloseContainerButton = document.getElementsByClassName(
     "rightSection-spaces-window-tabs-container-close-button"
 )
 const windowTabContainer = document.getElementsByClassName("rightSection-spaces-window-tabs-container")
-for (let i in Array.from(windowTabContainer)) {
-    windowTabsCloseButton[i].addEventListener("click", () => {
-        console.log(windowTabContainer)
-        console.log(windowTabsCloseContainer[i])
-        windowTabsCloseContainer[i].style.display = "flex"
-    })
-    windowTabsReturnContainerButton[i].addEventListener("click", () => {
-        windowTabsCloseContainer[i].style.display = "none"
-    })
-    windowTabsCloseContainerButton[i].addEventListener("click", () => {
-        windowTabContainer[i].parentNode.removeChild(windowTabContainer[i])
-    })
+function windowTabContainerAddEvent() {
+    for (let i in Array.from(windowTabContainer)) {
+        windowTabsCloseButton[i].addEventListener("click", () => {
+            console.log(windowTabContainer)
+            console.log(windowTabsCloseContainer[i])
+            windowTabsCloseContainer[i].style.display = "flex"
+        })
+        windowTabsReturnContainerButton[i].addEventListener("click", () => {
+            windowTabsCloseContainer[i].style.display = "none"
+        })
+        windowTabsCloseContainerButton[i].addEventListener("click", () => {
+            windowTabContainer[i].parentNode.removeChild(windowTabContainer[i])
+            windowTabContainerAddEvent()
+        })
+    }
 }
-// 拖曳功能
+/* 左右大區塊縮放 */
+let isRightSectionFoldButton = false
+let isLeftSectionFoldButton = false
+let rightSectionWidth
+let leftSectionWidth
+function checkRightSectionWidth() {
+    if (isRightSectionFoldButton) {
+        rightSectionWidth = "20px"
+    } else {
+        rightSectionWidth = "min-content"
+    }
+}
+function checkLeftSectionWidth() {
+    if (isLeftSectionFoldButton) {
+        leftSectionWidth = "20px"
+    } else {
+        leftSectionWidth = "min-content"
+    }
+}
+
+const rightSectionFoldButton = document.getElementsByClassName("rightSection-fold-button")
+const rightSectionContainer = document.getElementsByClassName("rightSection-container")
+const rightSection = document.getElementsByClassName("rightSection")
+const sectionBox = document.getElementsByClassName("sectionBox")
+rightSectionFoldButton[0].addEventListener("click", () => {
+    if (!isRightSectionFoldButton) {
+        isRightSectionFoldButton = true
+        checkLeftSectionWidth()
+        checkRightSectionWidth()
+        rightSectionFoldButton[0].style.left = "-20px"
+        rightSectionFoldButton[0].style.borderRadius = "99px"
+        rightSectionFoldButton[0].style.boxShadow = "rgb(112 112 140 / 15%) -2px 0px 2px"
+        rightSectionFoldButton[0].style.transform = "rotate(0deg)"
+        rightSectionContainer[0].style.opacity = "0.001"
+        rightSection[0].style.borderLeft = "none"
+        rightSection[0].style.boxShadow = "rgb(112 112 140 / 25%) 0px 2px 15px"
+        sectionBox[0].style.gridTemplateColumns = `${leftSectionWidth} minmax(0px, 1fr) ${rightSectionWidth}`
+    } else {
+        isRightSectionFoldButton = false
+        checkLeftSectionWidth()
+        checkRightSectionWidth()
+        rightSectionFoldButton[0].style.left = "5px"
+        rightSectionFoldButton[0].style.borderRadius = "none"
+        rightSectionFoldButton[0].style.boxShadow = "none"
+        rightSectionFoldButton[0].style.transform = "rotate(180deg)"
+        rightSectionContainer[0].style.opacity = "1"
+        rightSection[0].style.borderLeft = "1px solid rgb(73, 73, 92)"
+        rightSection[0].style.boxShadow = "none"
+        sectionBox[0].style.gridTemplateColumns = `${leftSectionWidth} minmax(0px, 1fr) ${rightSectionWidth}`
+    }
+})
+const leftSectionFoldButton = document.getElementsByClassName("leftSection-fold-button-container")
+const leftSection = document.getElementsByClassName("leftSection")
+const leftSectionSpaces = document.getElementsByClassName("leftSection-spaces")
+const leftSectionSpacesContainer = document.getElementsByClassName("leftSection-spaces-container")
+const leftSectionSpacesSearch = document.getElementsByClassName("leftSection-spaces-search")
+leftSectionFoldButton[0].addEventListener("click", () => {
+    if (!isLeftSectionFoldButton) {
+        isLeftSectionFoldButton = true
+        checkLeftSectionWidth()
+        checkRightSectionWidth()
+        leftSectionFoldButton[0].style.right = "-20px"
+        leftSectionFoldButton[0].style.borderRadius = "99px"
+        leftSectionFoldButton[0].style.boxShadow = "rgb(112 112 140 / 15%) -2px 0px 2px"
+        leftSectionFoldButton[0].style.transform = "rotate(180deg)"
+        leftSectionSpaces[0].style.boxShadow = "rgb(112 112 140 / 25%) 0px 2px 15px"
+        leftSection[0].style.transform = "translateX(-260px)"
+        leftSectionSpacesContainer[0].style.opacity = "0"
+        leftSectionSpacesSearch[0].style.opacity = "0"
+        sectionBox[0].style.gridTemplateColumns = `${leftSectionWidth} minmax(0px, 1fr) ${rightSectionWidth}`
+    } else {
+        isLeftSectionFoldButton = false
+        checkLeftSectionWidth()
+        checkRightSectionWidth()
+        leftSectionFoldButton[0].style.right = "5px"
+        leftSectionFoldButton[0].style.borderRadius = "none"
+        leftSectionFoldButton[0].style.boxShadow = "none"
+        leftSectionFoldButton[0].style.transform = "none"
+        leftSectionSpaces[0].style.boxShadow = "none"
+        leftSection[0].style.transform = "none"
+        leftSectionSpacesContainer[0].style.opacity = "1"
+        leftSectionSpacesSearch[0].style.opacity = "1"
+        sectionBox[0].style.gridTemplateColumns = `${leftSectionWidth} minmax(0px, 1fr) ${rightSectionWidth}`
+    }
+})
+/* 拖曳功能 */
